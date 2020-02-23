43347fde0c363a0c56b0a2f526272423634407a6
snakebiking49 <snakebiking49@gmail.com>
2020-02-22 22:50:03 -0800

    Machine Learning

---
+++ Machine Learning/predict.py
@@ -0,0 +1,24 @@
+# Imports
+import keras
+from keras.models import load_model as load_model_keras
+
+# load model function
+def load_model(path='./models/model_saves/Final_Model.model'):
+    model = load_model_keras(path)
+    return model
+
+# Shape image function
+def shape_image(image):
+    image = image.reshape((50, 50, 1))
+    image = np.expand_dims(image, axis=0)
+    return image
+
+# Prediction function
+def make_prediction(image, model):
+    return model.predict(image)
+
+# Utilized prediction function
+def predict(image, model):
+    shaped_image = shape_image(image)
+    prediction = make_prediction(image, model)
+    return prediction
